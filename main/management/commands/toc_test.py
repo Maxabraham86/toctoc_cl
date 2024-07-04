7060e098e892c50b80bc9fe380ba471d2b2bb3bc
Maximiliano Abraham <maxabrahamproyectos@gmail.com>
2024-07-03 21:23:40 -0400

    [ADD] se arreglan las migraciones y  la relacion entre usuarios , se pobla tabla mediante el archivo toc_test, se mejoran modelos y se agrega la clase UserProfile

--- main/management/commands/toc_test.py
+++ main/management/commands/toc_test.py
@@ -6,15 +6,20 @@ class Command(BaseCommand):
             #tipo=(('u','arrendatario'), ('p','arrendador'))
 
     def handle (self, *args, **kwargs):
-        nombre = Usuario.objects.create( 
-            nombre_usuario = 'R.R.',
-            nombres ="Roger",
-            apellidos ="Rabbit",
+        usuario = User.objects.create_user(
+            username = 'RR',
+            first_name = 'Roguer',
+            last_name = 'Rabit',
+            password = '1234',
+            email= 'aaa@bbb.ccc'
+        )
+        UserProfile.objects.create( 
             rut="15852258-3",
-            direccion="calle tinta, comuna navidad,  region,  Toonlandia",
+            direccion="calle tinta, comuna Toonlandia,  region Talca",
             telefono_personal='956482152',
-            correo_e = 'aaa@bbb.ccc',
-            tipo_usuario= 'arrendatiario'
+            user_type= 'arrendatario',
+            user=usuario,
+            
         )
 
 
--- main/models.py
+++ main/models.py
@@ -1,16 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Usuario(models.Model):
-    tipo=(('u','arrendatario'), ('p','arrendador'))
-    nombre_usuario= models.CharField(max_length=120, default='vacio')
-    nombres = models.CharField(max_length=255)
-    apellidos = models.CharField(max_length=255)
-    rut= models.CharField(max_length=10)
-    direccion= models.CharField(max_length=255)
-    telefono_personal= models.CharField(max_length=20)
-    correo_e = models.EmailField()
-    tipo_usuario= models.CharField(max_length=50, choices=tipo)
+class UserProfile(models.Model):
+    tipo_de_usuario = (
+        ('arrendador', 'Arrendador'),
+        ('arrendatario', 'Arrendatario'),
+    )
+    rut = models.CharField(max_length=12, blank=False)
+    direccion = models.CharField(max_length=255, blank=False)
+    telefono_personal = models.CharField(max_length=20, blank=False)
+    user_type = models.CharField(max_length=20, choices=tipo_de_usuario)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        nombre = self.user.first_name
+        apellido = self.user.last_name
+        usuario = self.user.username
+        tipo_usuario = self.tipo_de_usuario
+        return f'{nombre} {apellido} | {usuario} | {tipo_usuario}'
+    
+    
+# Create your models here.
+# class Usuario(models.Model):
+#     tipo=(('u','arrendatario'), ('p','arrendador'))
+#     nombre_usuario= models.CharField(max_length=120, default='vacio')
+#     nombres = models.CharField(max_length=255)
+#     apellidos = models.CharField(max_length=255)
+#     rut= models.CharField(max_length=10)
+#     direccion= models.CharField(max_length=255)
+#     telefono_personal= models.CharField(max_length=20)
+#     correo_e = models.EmailField()
+#     tipo_usuario= models.CharField(max_length=50, choices=tipo)
     
 
 class Inmueble(models.Model):
